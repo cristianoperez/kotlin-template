89b63f66347c343e4b6125f31dc74842517b3aeb
Diego Alves <diego.oborba@gmail.com>
2019-05-23 15:31:02 -0300

    adiciona camada web

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,23 +21,30 @@ plugins {
 group = "br.com.creditas"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    jcenter()
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-val compileKotlin by tasks.getting(KotlinCompile::class) {
-    kotlinOptions.jvmTarget = "1.8"
-    doLast { println("Finished compiling Kotlin source code") }
+allprojects {
+    repositories {
+        jcenter()
+    }
+
+    apply {
+        plugin("kotlin")
+    }
+
+    val compileKotlin by tasks.getting(KotlinCompile::class) {
+        kotlinOptions.jvmTarget = "1.8"
+        doLast { println("Finished compiling Kotlin source code") }
+    }
+
+    val compileTestKotlin by tasks.getting(KotlinCompile::class) {
+        kotlinOptions.jvmTarget = "1.8"
+        doLast { println("Finished compiling Kotlin source code for testing") }
+    }
 }
 
-val compileTestKotlin by tasks.getting(KotlinCompile::class) {
-    kotlinOptions.jvmTarget = "1.8"
-    doLast { println("Finished compiling Kotlin source code for testing") }
-}
 
 detekt {
     version = "1.0.0.RC8"
