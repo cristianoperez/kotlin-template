8c9c3c01e3e1a8d9e555e237e17c4d40b2a77658
fabriciorissetto <fabriciorissetto@gmail.com>
2019-05-23 17:56:36 -0300

    incluindo dependências do swagger

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
