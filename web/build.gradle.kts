d9a057972779c24380fdcd487e6bd7d2b976cbe5
Guilherme Pohlmann <guilherme.pr@outlook.com>
2019-05-24 14:54:58 -0300

    Adicionadas as dependencias. Criados application.properties por ambiente e ajustado build e testes.

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,3 +1,5 @@
+val SpringBootVersion = "2.1.5.RELEASE"
+
 plugins {
 	id("org.springframework.boot") version "2.1.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.7.RELEASE"
@@ -9,12 +11,16 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:$SpringBootVersion")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:$SpringBootVersion")
+	implementation("org.springframework.boot:spring-boot-starter-web:$SpringBootVersion")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.springfox:springfox-swagger2:2.9.2")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc:$SpringBootVersion")
+	runtimeOnly("org.postgresql:postgresql:42.2.5.jre7")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:$SpringBootVersion")
 }
 
