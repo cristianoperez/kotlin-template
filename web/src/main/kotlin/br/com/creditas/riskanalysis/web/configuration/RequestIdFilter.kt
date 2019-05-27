45117580d7353f644cd1ebc67ec33fc2d1990f42
Josias Straesser da SIlva <josiasstraesser@gmail.com>
2019-05-27 16:10:41 -0300

    adicionar request filter para adicionar aos logs

---
+++ web/src/main/kotlin/br/com/creditas/riskanalysis/web/configuration/RequestIdFilter.kt
@@ -0,0 +1,32 @@
+package rag.web.configuration
+
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.GenericFilterBean
+import java.util.UUID
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class RequestIdFilter : GenericFilterBean() {
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        if (request != null) {
+            val http = request as HttpServletRequest
+            MDC.put("request-id", getRequestId(http))
+        }
+
+        chain!!.doFilter(request, response)
+    }
+
+    private fun getRequestId(request: HttpServletRequest): String {
+        val requestId = request.getHeader("X-Request-ID")
+
+        return if (requestId != null) {
+            "$requestId,${UUID.randomUUID()}"
+        } else {
+            UUID.randomUUID().toString()
+        }
+    }
+}
