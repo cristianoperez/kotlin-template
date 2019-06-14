5b258ab64c448ecb588a252c043854c149e53777
Cristiano Perez <cristianoperez@gmail.com>
2019-06-14 20:18:45 -0300

    Delete TagRepository.kt

--- src/main/kotlin/br/com/creditas/kotlinspringsample/repositories/TagRepository.kt
+++
@@ -1,8 +0,0 @@
-package br.com.creditas.kotlinspringsample.repositories
-
-import br.com.creditas.kotlinspringsample.models.Post
-import br.com.creditas.kotlinspringsample.models.Tag
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface TagRepository : CrudRepository<Tag, UUID>
