2ae3617d5aa9587cbf0c2a0f8d110e8563b1aa7e
Antonio Sampaio <antonio.sampaio.n@gmail.com>
2019-07-10 16:17:30 -0300

    Adiciona campos teste do parse

--- src/main/kotlin/br/com/creditas/kotlinspringsample/models/SampleEntity.kt
+++ src/main/kotlin/br/com/creditas/kotlinspringsample/models/SampleEntity.kt
@@ -1,7 +1,10 @@
 package br.com.creditas.kotlinspringsample.models
 
+import java.time.LocalDateTime
 import java.util.UUID
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.Id
 import javax.persistence.Table
 
@@ -9,8 +12,17 @@ import javax.persistence.Table
 @Table(name = "sample_entity")
 data class SampleEntity(
     val title: String,
-    val description: String
+    val description: String,
+    val fullName: String,
+    val birthDate: LocalDateTime,
+
+    @Enumerated(EnumType.STRING)
+    val entityType: TypeEnum
 ) {
     @Id
     val id: UUID = UUID.randomUUID()
+}
+
+enum class TypeEnum {
+    SIMPLE, COMPLETE
 }
---
+++ src/main/resources/db/migration/V2__alter_table_sample_entity.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.sample_entity
+ADD COLUMN full_name VARCHAR,
+ADD COLUMN birth_date TIMESTAMP,
+ADD COLUMN entity_type VARCHAR;
