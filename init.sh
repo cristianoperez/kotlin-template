2162c66ec4c160156511146a23fddb1036be0291
Cristiano <cristianoperez@gmail.com>
2019-12-05 14:27:13 -0300

    Simplifica o template

--- init.sh
+++ init.sh
@@ -2,29 +2,14 @@
 
 set -e
 
-ENVIRONMENT_NAME="${SPRING_PROFILES_ACTIVE:-"development"}"
 JVM_OPS="${JVM_OPS:-""}"
 NEWRELIC_APP_NAME="${NEWRELIC_APP_NAME:-"kotlin_spring_sample"}"
 echo "ENVIRONMENT_NAME: ${ENVIRONMENT_NAME}"
 
-COMMAND=${1:-"web"}
-echo $COMMAND
-
-case "$COMMAND" in
-  migrate|web)
-    # NOTE: NewRelic won't work unless -javaagent is the first argument
-    exec java ${JVM_OPS} -Djava.security.egd=file:/dev/./urandom \
-      -javaagent:/app/newrelic.jar \
-      -Duser.Timezone=America/Sao_Paulo \
-      -Dnewrelic.config.license_key=${NEWRELIC_TOKEN} \
-      -Dnewrelic.config.app_name=${NEWRELIC_APP_NAME} \
-      -Dnewrelic.config.distributed_tracing.enabled=true \
-      -Dspring.profiles.active=${ENVIRONMENT_NAME} \
-      -Dspring.datasource.url=${DATABASE_URL} \
-      -jar /app/kotlin-spring-sample-*.jar \
-      $COMMAND
-    ;;
-  *)
-    exec sh -c "$*"
-    ;;
-esac
+exec java $JVM_OPS -Djava.security.egd=file:/dev/./urandom \
+  -javaagent:/app/newrelic.jar \
+  -Duser.Timezone=America/Sao_Paulo \
+  -Dnewrelic.config.license_key=$NEWRELIC_TOKEN \
+  -Dnewrelic.config.app_name=$NEWRELIC_APP_NAME \
+  -Dnewrelic.config.distributed_tracing.enabled=true \
+  -jar /app/kotlin-spring-sample-*.jar
